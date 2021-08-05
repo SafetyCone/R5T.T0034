8d36901d95861ec7a85fd8dcb856db482ced2513
David Coats <david@getsafetycone.com>
2021-08-05 13:15:47 -0700

    Modify project plan, add documentation.

--- source/R5T.T0034/Code/Documentation.cs
+++ source/R5T.T0034/Code/Documentation.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+
 namespace R5T.T0034
 {
+    /// <summary>
+    /// <see cref="ITypeName"/>, <see cref="INamespacedTypeName"/> extension method base types.
+    /// </summary>
     public class Documentation
     {
     }
