83da2f28ae5ec8046e9a57136b502cf949877158
David Coats <david@getsafetycone.com>
2021-09-13 14:19:34 -0700

    Add new type name values.

--- source/R5T.T0034.X011/Code/Bases/Extensions/ITypeNameExtensions.cs
+++ source/R5T.T0034.X011/Code/Bases/Extensions/ITypeNameExtensions.cs
@@ -8,9 +8,24 @@ namespace System
 {
     public static class ITypeNameExtensions
     {
+        public static string Class1(this ITypeName _)
+        {
+            return TypeNames.Class1;
+        }
+
+        public static string Interface1(this ITypeName _)
+        {
+            return TypeNames.Interface1;
+        }
+
         public static string Program(this ITypeName _)
         {
             return TypeNames.Program;
+        }
+
+        public static string Startup(this ITypeName _)
+        {
+            return TypeNames.Startup;
         }
     }
 }
--- source/R5T.T0034.X011/Code/Constants/TypeNames.cs
+++ source/R5T.T0034.X011/Code/Constants/TypeNames.cs
@@ -5,6 +5,9 @@ namespace R5T.T0034.X011
 {
     public static class TypeNames
     {
+        public static string Class1 => "Class1";
+        public static string Interface1 => "Interface1";
         public static string Program => "Program";
+        public static string Startup => "Startup";
     }
 }
