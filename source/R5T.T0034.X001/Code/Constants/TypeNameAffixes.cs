dc6af8b0593987bc78acc8ced0f69c1d8b28ccfd
David Coats <david@getsafetycone.com>
2021-09-28 15:19:28 -0700

    Move ITypeNameAffixExtensions code files, add IAttributeTypeName extensions.

---
+++ source/R5T.T0034.X001/Code/Constants/TypeNameAffixes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.T0034.X001
+{
+    public static class TypeNameAffixes
+    {
+        public static string AttributeSuffix => "Attribute";
+    }
+}
---
+++ source/R5T.T0034.X001/Code/Extensions/IAttributeTypeNameExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using R5T.Magyar;
+
+using R5T.T0034;
+using R5T.T0034.X001;
+
+
+namespace System
+{
+    public static class IAttributeTypeNameExtensions
+    {
+        public static string GetAttributeSuffixedTypeName(this IAttributeTypeName _,
+            string typeName)
+        {
+            var output = $"{typeName}{TypeNameAffixes.AttributeSuffix}";
+            return output;
+        }
+
+        public static string GetEnsuredAttributeSuffixedTypeName(this IAttributeTypeName _,
+            string typeName)
+        {
+            var isAttributeSuffixedTypeName = _.IsAttributeSuffixedTypeName(typeName);
+            if(!isAttributeSuffixedTypeName)
+            {
+                var output = _.GetAttributeSuffixedTypeName(typeName);
+                return output;
+            }
+            else
+            {
+                return typeName;
+            }
+        }
+
+        public static string GetEnsuredNonAttributeSuffixedTypeName(this IAttributeTypeName _,
+            string typeName)
+        {
+            var isAttributeSuffixedTypeName = _.IsAttributeSuffixedTypeName(typeName);
+            if (isAttributeSuffixedTypeName)
+            {
+                var output = _.GetNonAttributeSuffixedTypeName(typeName);
+                return output;
+            }
+            else
+            {
+                return typeName;
+            }
+        }
+
+        public static string GetNonAttributeSuffixedTypeName(this IAttributeTypeName _,
+            string attributeSuffixedTypeName)
+        {
+            var output = attributeSuffixedTypeName.ExceptEnding(TypeNameAffixes.AttributeSuffix);
+            return output;
+        }
+
+        public static bool IsAttributeSuffixedTypeName(this IAttributeTypeName _,
+            string typeName)
+        {
+            var output = typeName.EndsWith(TypeNameAffixes.AttributeSuffix);
+            return output;
+        }
+    }
+}
